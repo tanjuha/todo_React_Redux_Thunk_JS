40d78369f2057bcc9b7a40a5a6886f200c0c3a6b
Tetiana Kovalenko <tkovale@softserveinc.com>
2021-07-29 14:48:27 +0300

    add value to localStorage

--- src/components/SingIn/SingIn.js
+++ src/components/SingIn/SingIn.js
@@ -6,7 +6,9 @@ export default class SingIn extends Component {
   
   handleSubmit = (values) => {
     // Do something with the form values
-    console.log(values);
+    localStorage.setItem('user', JSON.stringify(values));
+    localStorage.setItem('isAuthenticated', true);
+    this.props.history.push("/")
   }
   render() {
     return (
@@ -17,25 +19,22 @@ export default class SingIn extends Component {
   }
 }
 
-class SingInForm extends Component {
-  render() {
-    const { handleSubmit } = this.props;
-    return (
-      <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label>Email</label>
-        <Field name="email"  className="form-control" component="input" type="text"/>
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <Field name="password"  className="form-control" component="input" type="password"/>
-      </div>
-      <button type="submit" className="btn btn-primary btn-block">
-        Submit
-      </button>
-    </form>
-    )
-  }
+let SingInForm = (props) =>  {
+  return (
+    <form onSubmit={props.handleSubmit}>
+    <div className="form-group">
+      <label>Email</label>
+      <Field name="email"  className="form-control" component="input" type="text"/>
+    </div>
+    <div className="form-group">
+      <label>Password</label>
+      <Field name="password"  className="form-control" component="input" type="password"/>
+    </div>
+    <button type="submit" className="btn btn-primary btn-block">
+      Submit
+    </button>
+  </form>
+  )
 }
 
 
