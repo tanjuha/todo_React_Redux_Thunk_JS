ee7a78d020498cfc1571467cbf09f54a6ce474fc
Tetiana Kovalenko <tkovale@softserveinc.com>
2021-08-11 11:59:55 +0300

    added propTypes for alert

--- src/components/common/Alert/Alert.js
+++ src/components/common/Alert/Alert.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import "./alert.css";
 import { cleareMesage } from "../../../actions/todosActions";
 import { connect } from "react-redux";
-
-function Alert(props) {
+import propTypes from 'prop-types'
+function Alert({cleareMesage, id, status, text}) {
   const [isShow, setShow] = useState(true);
 
   useEffect(() => {
     const timeShowMsg = setTimeout(() => {
       setShow(false);
-      props.cleareMesage(props.id);
+      cleareMesage(id);
     }, 3000);
 
     return () => {
@@ -18,11 +18,20 @@ function Alert(props) {
   });
 
   return isShow ? (
-    <div className={`alert alert-${props.status} notification`} role="alert">
-      {props.text}
+    <div className={`alert alert-${status} notification`} role="alert">
+      {text}
     </div>
   ) : null;
 }
+
+Alert.prototype = {
+  cleareMesage: propTypes.func,
+  id: propTypes.number,
+  status: propTypes.string,
+  text: propTypes.string
+}
+
+
 
 function mapDispatchToProps(dispatch) {
   return {
